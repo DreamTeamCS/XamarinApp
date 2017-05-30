95d209fbf1e74623661d374fda774d4d06d6e60d
lupacja14 <lupacja14@sps-prosek.cz>
2017-05-30 08:45:38 +0200

    cc

---
+++ XamarinApp/XamarinApp/Results.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class Results : ContentPage
+    {
+        public Results()
+        {
+            InitializeComponent();
+        }
+    }
+}
